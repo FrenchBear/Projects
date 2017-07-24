9dfbfab45e4cd5081c9d6308510157d891a521f7
FrenchBear (Higgs) <FrenchBear38@outlook.com>
2017-07-25 00:20:47 +0200

    Project folders restructured

--- Bonza/Editor/Model/BonzaModel.cs
+++ Bonza/Editor/Model/BonzaModel.cs
@@ -27,7 +27,7 @@ namespace Bonza.Editor
         {
             // Demo
             Grille g = new Grille();
-            for (; !g.PlaceWords("ConstellationsFrench.txt");)
+            for (; !g.PlaceWords("Fruits.txt");)
             {
             }
             Layout = g.GetLayout();
