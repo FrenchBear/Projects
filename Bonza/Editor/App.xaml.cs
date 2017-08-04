84cfc17cff0d981c22a096ca5bddb3bfe7331992
FrenchBear (Higgs) <FrenchBear38@outlook.com>
2017-08-05 00:46:47 +0200

    const added

--- Bonza/Editor/App.xaml.cs
+++ Bonza/Editor/App.xaml.cs
@@ -8,12 +8,12 @@ namespace Bonza.Editor
     /// </summary>
     public partial class App : Application
     {
-        internal static readonly string AppName = "BonzaEditor";
+        internal const string AppName = "BonzaEditor";
 
         // Size of the side of a square
         // Background grid is drawn using lines of width 1 (or 3 for origin axes) [hardcoded]
         // Font size is 16 and padding are also hardcoded
-        internal static readonly double UnitSize = 25.0;
+        internal const double UnitSize = 25.0;
 
 
         // Some colors
