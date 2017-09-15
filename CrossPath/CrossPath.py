2d2add05616b2860b6e45ff2ce9608e695d1c665
FrenchBear <FrenchBear38@outlook.com>
2017-09-15 11:01:43 +0200

    2nd step of easy strategy implemented

--- CrossPath/CrossPath.py
+++ CrossPath/CrossPath.py
@@ -3,20 +3,10 @@
 
 from puzzle import *
 
-"""
-.6....2
-.....X.
-..1.4..
-...4.X.
-3.4....
-.4....4
-X...3.X
-
-Cellule
-1-9	Centre de réserve d’extension
-.	libre
-X	cellule bloquée
-"""
+# Les puzzles sont définis comme une liste de chaînes, une par ligne, charque caractère de la chaîne étant:
+# 1-9 Centre de réserve d’extension
+# .   libre
+# X   cellule bloquée
 
 medium_10 = [
 ".6....2",
@@ -41,12 +31,20 @@ master_1 = [
 ".X...X...1.",
 "..2X.XX...6"]
 
+test_4 = [
+"2.X1",
+"..X.",
+".1.2",
+"3...",
+]
+
+
 
 #p = puzzle(medium_10)
-p = puzzle(master_1)
+#p = puzzle(master_1)
+p = puzzle(test_4)
 
 print(str(p))
 print()
 p.solve(False)
 print(str(p))
-
--- CrossPath/puzzle.py
+++ CrossPath/puzzle.py
@@ -16,7 +16,6 @@ X	Blocked cell
 Stratégie
 - Tant qu'il existe des cellules joignables par une seule extension, mettre en oeuvre cette extension
 - S'il existe des cellules extensibles d'une seule manière, mettre en oeuvre cette extension
-(Ne pas tenir compte des extensions qui pourraient poser problème)
 """
 
 class puzzle(object):
@@ -31,6 +30,12 @@ class puzzle(object):
         self.dicmap = { 'N':'▲', 'S':'▼', 'E':'►', 'W':'◄', '.':'·', 'X':'▒', '0':'◌'}
         self.dicmap.update({str(n):str(n) for n in range(1, 10)})
 
+        # Check that count of empty cells equals total of extensions
+        ns = [e for r in self.p for e in r].count(".")
+        te = sum([int(e) if e>="1" and e<="9" else 0 for r in self.p for e in r])
+        assert ns==te
+
+
     def __str__(self):
         return '\r\n'.join(' '.join(self.dicmap[c] for c in l) for l in self.p)
 
@@ -40,8 +45,6 @@ class puzzle(object):
     def GetExtensionsToCell(self, rTarget, cTarget):
         assert self.p[rTarget][cTarget]=="."
         l = []
-
-        trace = rTarget==0 and cTarget==3
 
         # search for horizontal extensions
         for c in range(self.columns):
@@ -88,6 +91,39 @@ class puzzle(object):
         return l
 
 
+    def ExploreExt(self, rSource, cSource, rDelta, cDelta, extension):
+        vuse = 0
+        vnum = int(self.p[rSource][cSource])
+        r,c = rSource,cSource
+        while vnum>0:
+            r += rDelta
+            c += cDelta
+            if r>=self.rows or r<0 or c>=self.columns or c<0: break
+            if self.p[r][c]!="." and self.p[r][c]!=extension: break
+            if self.p[r][c]==".": 
+                vuse += 1
+                vnum -= 1
+        if vuse>0:
+            return (rDelta, cDelta, extension, vuse)
+        else:
+            return None
+
+    def GetExtensionsFromCell(self, rSource, cSource):
+        v = self.p[rSource][cSource]
+        assert v>="0" and v<="9"
+        l = []
+
+        # Look in all 4 directions
+        ext = self.ExploreExt(rSource, cSource, -1, 0, "N")
+        if ext: l.append(ext)
+        ext = self.ExploreExt(rSource, cSource, 1, 0, "S")
+        if ext: l.append(ext)
+        ext = self.ExploreExt(rSource, cSource, 0, -1, "W")
+        if ext: l.append(ext)
+        ext = self.ExploreExt(rSource, cSource, 0, 1, "E")
+        if ext: l.append(ext)
+        return l
+
     def solve(self, showSteps):
         doAgain = True
         while doAgain:
@@ -97,9 +133,9 @@ class puzzle(object):
                     if self.p[r][c]==".":
                         l = self.GetExtensionsToCell(r, c)
                         if len(l)==1:
-                            # Apply extension l[0]
+                            # Only one extension possible to cell (r,c): do it
                             if showSteps:
-                                print((r, c), ": ", l[0])
+                                print("To ", (r, c), ": ", l[0])
                             rSource = l[0][0]
                             cSource = l[0][1]
                             rDelta = l[0][2]
@@ -122,4 +158,33 @@ class puzzle(object):
                                 print()
                             doAgain = True
                             break
+
+                    if self.p[r][c]>="1" and self.p[r][c]<="9":
+                        l = self.GetExtensionsFromCell(r, c)
+                        if len(l)==1:
+                            # Only one extension direction possible that exhaust remaining count: do it
+                            if showSteps:
+                                print("From ", (r, c), ": ", l[0])
+                            rDelta = l[0][0]
+                            cDelta = l[0][1]
+                            extChar = l[0][2]
+                            vuse = l[0][3]
+                            assert int(self.p[r][c])==vuse
+                            r1,c1 = r,c
+                            while vuse>0:
+                                r1 += rDelta
+                                c1 += cDelta
+                                if r>=self.rows or r<0 or c>=self.columns or c<0: break
+                                if self.p[r1][c1]!="." and self.p[r1][c1]!=extChar: break
+                                if self.p[r1][c1]==".": 
+                                    vuse -= 1
+                                    self.p[r1][c1] = extChar
+                            assert vuse==0
+                            self.p[r][c] = "0"
+                            if showSteps:
+                                print(str(self))
+                                print()
+                            doAgain = True
+                            break
+
                 if doAgain: break
