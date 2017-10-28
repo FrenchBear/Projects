0d40f2e7a2b5b90a5a4e31d9e53c97f2ceb63cae
FrenchBear (Higgs) <FrenchBear38@outlook.com>
2017-10-28 14:35:01 +0200

    changements dans le train

---
+++ Linedoku/DetectClosedArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectClosedArea
+{
+    class Program
+    {
+        static void Main()
+        {
+            var t = new TestBoard(7);
+            t.Fill();
+            t.Print();
+            t.FindAreas(0);
+
+
+            Console.WriteLine();
+            Console.Write("(Pause)");
+            Console.ReadLine();
+        }
+    }
+}
