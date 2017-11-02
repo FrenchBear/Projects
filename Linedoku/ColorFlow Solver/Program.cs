c2bbc1220b3b9dcc0b4105d12803450d21245e83
French Bear <FrenchBear38@outlook.com>
2017-11-03 00:05:41 +0100

    Final commit

--- Linedoku/ColorFlow Solver/Program.cs
+++ Linedoku/ColorFlow Solver/Program.cs
@@ -23,7 +23,7 @@ namespace ColorFlow_Solver
             OutputEncoding = Encoding.UTF8;
 
             var n = DateTime.Now;
-            using (StreamWriter swu = new StreamWriter(string.Format(@"C:\temp\ColorFlow-{0}-{1:D2}-{2:D2}-{3:D2}.{4:D2}.{5:D2}.log", n.Year, n.Month, n.Day, n.Hour, n.Minute, n.Second)))
+            using (StreamWriter swu = new StreamWriter(string.Format(@"C:\temp\ColorFlow-{0}-{1:D2}-{2:D2}-{3:D2}.{4:D2}.{5:D2}.txt", n.Year, n.Month, n.Day, n.Hour, n.Minute, n.Second)))
             {
                 swu.AutoFlush = true;
                 sw = swu;
@@ -32,10 +32,10 @@ namespace ColorFlow_Solver
 
                 //Board(5, new Line[] { new Line(0, 0, 4, 4), new Line(0, 1, 1, 3), new Line(2, 1, 4, 0), new Line(3, 1, 4, 3) });
                 //Board(7, new Line[] { new Line(0, 0, 4, 5), new Line(1, 0, 3, 3), new Line(2, 4, 6, 3), new Line(3, 2, 4, 2), new Line(5, 5, 6, 4) });
-                Board(9, new Line[] { new Line(0, 0, 4, 5), new Line(1, 3, 3, 8), new Line(1, 7, 5, 6), new Line(2, 0, 2, 5), new Line(3, 0, 8, 4), new Line(4, 8, 8, 5), new Line(6, 2, 7, 7) });
-                //Board(11, new Line[] { new Line(0, 0, 5, 5), new Line(1, 0, 2, 5), new Line(1, 3, 7, 9), new Line(1, 7, 3, 10), new Line(2, 10, 6, 9), new Line(5, 4, 10, 10), new Line(6, 1, 8, 3), new Line(7, 5, 9, 5), new Line(9, 1, 10, 9) });
+                //Board(9, new Line[] { new Line(0, 0, 4, 5), new Line(1, 3, 3, 8), new Line(1, 7, 5, 6), new Line(2, 0, 2, 5), new Line(3, 0, 8, 4), new Line(4, 8, 8, 5), new Line(6, 2, 7, 7) });
+                Board(11, new Line[] { new Line(0, 0, 5, 5), new Line(1, 0, 2, 5), new Line(1, 3, 7, 9), new Line(1, 7, 3, 10), new Line(2, 10, 6, 9), new Line(5, 4, 10, 10), new Line(6, 1, 8, 3), new Line(7, 5, 9, 5), new Line(9, 1, 10, 9) });
 
-                Solve(false);
+                Solve(true);
                 //TestsClosedAreas();
 
                 stw.Stop();
