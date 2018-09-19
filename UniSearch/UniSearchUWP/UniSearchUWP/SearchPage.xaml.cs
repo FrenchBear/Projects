33f1c256ef49d172a950cccf120f312751768c67
French Bear <FrenchBear38@outlook.com>
2018-09-19 13:29:48 +0200

    Tooltips and accelerators

--- UniSearch/UniSearchUWP/UniSearchUWP/SearchPage.xaml.cs
+++ UniSearch/UniSearchUWP/UniSearchUWP/SearchPage.xaml.cs
@@ -37,5 +37,22 @@ namespace UniSearchUWP
         {
             vm.NotifyPropertyChanged(nameof(vm.SelChars));
         }
+
+        // Some controls such as AutoSuggestBox do not get focused just by defining AccessKey property for some reason.
+        // This generic helper launched by AccessKeyInvoked event makes it work.
+        private void SetFocus(UIElement sender, Windows.UI.Xaml.Input.AccessKeyInvokedEventArgs args)
+        {
+            if (sender is Control c)
+                c.Focus(FocusState.Keyboard);
+        }
+
+        // Very, very stupid: can't find how to bind command to keyboard accelerator at page level in UWP.
+        // Can create bindings on top level control such as Page main grid... but can only lauch an event, not a command!!!
+        // So this event actually launches a command...
+        private void KeyboardAccelerator_CopyRecordsInvoked(Windows.UI.Xaml.Input.KeyboardAccelerator sender, Windows.UI.Xaml.Input.KeyboardAcceleratorInvokedEventArgs args)
+        {
+            if (vm.CopyRecordsCommand.CanExecute("1"))
+                vm.CopyRecordsCommand.Execute("1");
+        }
     }
 }
