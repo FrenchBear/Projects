2a1bdf669dda121eef8d921d249ca1354017f212
French Bear <FrenchBear38@outlook.com>
2019-04-29 22:12:17 +0200

    Git Projects folder reorg

--- 03 DotNet Core 2.0/AppAndLib/Run.bat
+++
@@ -1,9 +0,0 @@
-rem dotnet new lib -o mylibrary
-rem dotnet new web -o aspnetcore
-rem dotnet add reference ../mylibrary/mylibrary.csproj
-pushd aspnetcore
-dotnet run
-popd
-
-rem on a separate window,
-rem start http://localhost:5000
--- 03 DotNet Core 2.0/AppAndLib/mylibrary/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace mylibrary
-{
-    public class Class1
-    {
-		public static string GetMessage() => "Hello from .NET Standard!";
-    }
-}
