4a6d15e6b083666e5f2a6dce5235464d04408f9f
French Bear <FrenchBear38@outlook.com>
2020-11-16 13:37:43 +0100

    RelayCommand.cs support for #nullable enable

--- 03 UniSearch/UniSearchCore/UniSearchCore/RelayCommand.cs
+++ 03 UniSearch/UniSearchCore/UniSearchCore/RelayCommand.cs
@@ -1,46 +1,54 @@
 ﻿// UnISearch RelayCommand for MVVM CommandBinding
 // 2016-09-26   PV
-// 2020-11-11   PV      Support of #nullable enable too difficult here
+// 2020-01-02   PV  v3 Not Net Core
+// 2020-11-13   PV  Full support for #nullable enable
+
+#nullable enable
 
 using System;
 using System.Windows.Input;
 
+#nullable enable
 
 namespace UniSearchNS
 {
     internal class RelayCommand<T> : ICommand
     {
-        private readonly Predicate<T> canExecute;
+        private readonly Predicate<T>? canExecute;
         private readonly Action<T> execute;
 
-        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        public RelayCommand(Action<T> execute, Predicate<T>? canExecute)
         {
             this.canExecute = canExecute;
             this.execute = execute;
         }
 
         // canExecute is optional, and by default is assumed returning true (directly in CanExecute)
-        public RelayCommand(Action<T> execute)
-            : this(execute, null)
+        public RelayCommand(Action<T> execute) : this(execute, null)
         { }
 
         /* From ICommand */
 
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
-            return canExecute?.Invoke((T)parameter) ?? true;
+            if (canExecute == null)
+                return true;
+            if (parameter == null)
+                return canExecute(default!);
+            else
+                return canExecute((T)parameter);
         }
 
         /* From ICommand */
 
-        public void Execute(object parameter)
+        public void Execute(object? parameter)
         {
-            execute?.Invoke((T)parameter);
+            execute?.Invoke((T)parameter!);
         }
 
         // The 'black magic' part: according to help, CommandManager.RequerySuggested Event occurs when the
         // CommandManager """detects conditions that might change the ability of a command to execute"""...
-        // OK, it works, but exactly how does this detection works is still a mystery to me...
+        // Ok, it works, but exactly how does this detection works is still a mystery to me...
         //
         // Added info from CommandManager.InvalidateRequerySuggested Method:
         // The CommandManager only pays attention to certain conditions in determining when the command target has changed,
@@ -50,10 +58,10 @@ namespace UniSearchNS
 
         /* From ICommand */
 
-        public event EventHandler CanExecuteChanged
+        public event EventHandler? CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
     }
 }
