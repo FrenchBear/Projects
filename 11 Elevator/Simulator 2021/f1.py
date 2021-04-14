8a4162bd986ea06578189762aaddbac90946fa65
French Bear <FrenchBear38@outlook.com>
2021-04-15 01:27:27 +0200

    sim11, looks good!

--- 11 Elevator/Simulator 2021/f1.py
+++ 11 Elevator/Simulator 2021/f1.py
@@ -1,2 +1,21 @@
 l = ['a','b','c']
-print(l.index('d'))
+print(l.index('b'))
+
+class Actor:
+    pass
+
+class RoadItem(Actor):
+    pass
+
+class CarTarget(RoadItem):
+    pass
+
+ct = CarTarget()
+print(type(ct).__name__)
+print(ct.__class__.__name__)
+
+
+d = {1.414: '√2', 3.1416: 'pi'}
+for a,b in d.items():
+    print(a, b)
+    
