f2a1b71f00caeddf5472ed980b89269bf1e2fa51
French Bear <FrenchBear38@outlook.com>
2022-01-17 09:27:34 +0100

    save

--- 09_TangenteHS/Align_Pages/DetectYellow3.py
+++ 09_TangenteHS/Align_Pages/DetectYellow3.py
@@ -123,6 +123,39 @@ def p(file, numfile, width, height, colp, rowp, colback):
     newpage = ScannedPage(file, numfile, width, height, colp, rowp, colback)
     Pages.append(newpage)
 
+def clean_borders(img: np.ndarray):
+    height = img.shape[0]
+    width = img.shape[1]
+
+    def rgb2gray1(rgb):
+        return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+    def rgb2gray2(rgb):
+        return np.dot(rgb[...,:3], [0.2125, 0.7154, 0.0721])
+
+    gray = rgb2gray2(img)
+
+    def clean_boder(x1, x2):
+        step = 1 if x2>x1 else -1
+        cc = -1
+        for col in range(x1, x2, step):
+            avg = np.average(gray[:,col])
+            cnt = (gray[:, col] < 215).sum()
+            #print(col, avg, cnt)
+            if avg<220 or cnt>450:
+                break
+            cc += 1
+        return cc
+
+    bw = 30
+    left = clean_boder(0, bw)
+    right = clean_boder(width-1, width-1-bw)
+
+    if left>0:
+        img[:,:left+1] = [255,255,255]
+    if right>0:
+        img[:,-right-1:] = [255,255,255]
+
 
 # Step 1, determine sizes and position of yellow rect
 def process_root(root:str):
@@ -149,9 +182,9 @@ def process_root(root:str):
         print()
     
     else:
-        p(file='D:\\Scans\\THS75\\02Redresse\\THS75-001.jpg', numfile=1, width=1984, height=2849, colp=None, rowp=None)
+        p(file='D:\\Scans\\ToDo\\THS33B\\02Redresse\\THS33-001.jpg', numfile=1, width=1941, height=2826, colp=None, rowp=None, colback=None)
         # ...
-        p(file='D:\\Scans\\THS75\\02Redresse\\THS75-178.jpg', numfile=178, width=1995, height=2813, colp=None, rowp=None)
+        p(file='D:\\Scans\\ToDo\\THS33B\\02Redresse\\THS33-160.jpg', numfile=160, width=1943, height=2834, colp=None, rowp=None, colback=None)
 
     # Determinate median value for cropping
     widths = []
@@ -173,6 +206,10 @@ def process_root(root:str):
     rowp_m = int(statistics.median(x for x in rowps if x))
     colpp_m = int(statistics.median(x for x in colps_p if x))
     colpi_m = int(statistics.median(x for x in colps_i if x))
+
+    # Finally fixed the value, identical for all books
+    width_m = 1950
+    height_m = 2830
 
     print(f'{width_m=}')
     print(f'{height_m=}')
@@ -202,8 +239,8 @@ def process_root(root:str):
             target = os.path.join(dest, 'i', filename)
 
         # If the target already exist and is more recent than source, no need to do it again
-        if os.path.isfile(target) and os.path.getmtime(target) > os.path.getmtime(page.file):
-            continue
+        # if os.path.isfile(target) and os.path.getmtime(target) > os.path.getmtime(page.file):
+        #     continue
 
         img = mpimg.imread(page.file)[:, :, :3]
         width: int = img.shape[1]
@@ -229,16 +266,15 @@ def process_root(root:str):
             rins = width_m-page.width-lins
 
         img = cropimage.crop_image(img, tins, bins, lins, rins)
+        clean_borders(img)
         mpimg.imsave(target, img, format='jpg', dpi=300)
 
 
-# process_root('D:\Scans\THS32')
-# process_root('D:\Scans\THS39')
-# process_root('D:\Scans\THS61')
-# process_root('D:\Scans\THS62')
-# process_root('D:\Scans\THS63')
-# process_root('D:\Scans\THS64')
-# process_root('D:\Scans\THS65')
-# process_root('D:\Scans\THS66')
-# process_root('D:\Scans\THS68')
-process_root('D:\Scans\THS69')
+#process_root("D:\Scans\ToDo\THS33B")
+process_root("D:\Scans\ToDo\THS70")
+process_root("D:\Scans\ToDo\THS71")
+process_root("D:\Scans\ToDo\THS72")
+process_root("D:\Scans\ToDo\THS73")
+process_root("D:\Scans\ToDo\THS74")
+process_root("D:\Scans\ToDo\THS75")
+process_root("D:\Scans\ToDo\THS76")
