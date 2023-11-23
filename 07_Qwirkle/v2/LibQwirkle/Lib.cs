4e0577f4945dae1c0d7451def23fece5f3415320
French Bear <FrenchBear38@outlook.com>
2023-11-23 23:49:23 +0100

    Separated LibQwirkle

---
+++ 07_Qwirkle/v2/LibQwirkle/Lib.cs
@@ -0,0 +1,211 @@
+﻿// LibQwirkle
+// Core library for Qwirkle reboot
+//
+// 2023-11-23   PV
+
+using System.Diagnostics;
+using System.Text;
+
+namespace LibQwirkle;
+
+public enum Shape
+{
+    Circle,
+    Cross,
+    Lozange,
+    Square,
+    Star,
+    Clover,
+}
+
+public enum Color
+{
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Purple,
+}
+
+public record Tile(Color C, Shape S, int Instance)
+{
+    public Color Color { get; } = C;
+    public Shape Shape { get; } = S;
+    public int Instance { get; } = Instance;
+}
+
+public record Move(int Row, int Col, Tile T)
+{
+    public int Row { get; } = Row;
+    public int Col { get; } = Col;
+    public Tile Tile { get; } = T;
+}
+
+public enum CellState
+{
+    EmptyIsolated,
+    PotentiallyPlayable,
+    Tiled,
+}
+
+public class Board
+{
+    readonly Board? BaseBoard = null;
+    readonly List<Move> Moves = [];
+
+    public int RowMin { get; private set; } = 100;
+    public int RowMax { get; private set; } = 0;
+    public int ColMin { get; private set; } = 100;
+    public int ColMax { get; private set; } = 0;
+
+    public void AddMove(Move m)
+    {
+        Moves.Add(m);
+        RowMin = Math.Min(RowMin, m.Row);
+        RowMax = Math.Max(RowMax, m.Row);
+        ColMin = Math.Min(ColMin, m.Col);
+        ColMax = Math.Max(ColMax, m.Col);
+    }
+
+    public Tile? GetTile(int row, int col)
+    {
+        foreach (var t in Moves)
+            if (t.Row == row && t.Col == col)
+                return t.Tile;
+        if (BaseBoard == null)
+            return null;
+        return BaseBoard.GetTile(row, col);
+    }
+
+    public CellState GetCellState(int row, int col)
+    {
+        Tile? t = GetTile(row, col);
+        if (t != null)
+            return CellState.Tiled;
+
+        Tile? tn = GetTile(row - 1, col);
+        Tile? ts = GetTile(row + 1, col);
+        Tile? te = GetTile(row, col + 1);
+        Tile? tw = GetTile(row, col - 1);
+        if (tn == null && ts == null && te == null && tw == null)
+            return CellState.EmptyIsolated;
+        return CellState.PotentiallyPlayable;
+    }
+
+    public bool IsCompatible(int row, int col, in Tile t)
+    {
+        Debug.Assert(GetTile(row, col) == null);
+        if (!IsDirectionCompatible(row, col, -1, 0, in t, out bool nn))
+            return false;
+        if (!IsDirectionCompatible(row, col, 1, 0, in t, out bool sn))
+            return false;
+        if (!IsDirectionCompatible(row, col, 0, 1, in t, out bool en))
+            return false;
+        if (!IsDirectionCompatible(row, col, 0, -1, in t, out bool wn))
+            return false;
+        Debug.Assert(nn | sn | en | wn);    // Check that we have at least one neighbor
+        return true;
+    }
+
+    private bool IsDirectionCompatible(int row, int col, int deltaRow, int deltaCol, in Tile refTile, out bool neighborFound)
+    {
+        neighborFound = false;
+        for (; ; )
+        {
+            row += deltaRow;
+            col += deltaCol;
+            var tmpTile = GetTile(row, col);
+            if (tmpTile == null)
+                return true;
+            neighborFound = true;
+            if (tmpTile.Color != refTile.Color && tmpTile.Shape != refTile.Shape)
+                return false;
+            if (tmpTile.Color == refTile.Color && tmpTile.Shape == refTile.Shape)
+                return false;
+        }
+    }
+
+    public void Print() => Console.WriteLine(AsString(true));
+
+    const string ConsoleColorRed = "\x1b[91m";
+    const string ConsoleColorDarkYellow = "\x1b[33m";
+    const string ConsoleColorYellow = "\x1b[93m";
+    const string ConsoleColorGreen = "\x1b[92m";
+    const string ConsoleColorBlue = "\x1b[94m";
+    const string ConsoleColorMagenta = "\x1b[95m";
+    const string ConsoleColorCyan = "\x1b[96m";
+    const string ConsoleColorDarkGray = "\x1b[90m";
+    const string ConsoleColorDefault = "\x1b[39m";
+
+    public string AsString(bool color, Tile? checkTileCompat = null)
+    {
+        var sb = new StringBuilder();
+
+        sb.Append("   ");
+        for (int col = ColMin - 1; col <= ColMax + 1; col++)
+            sb.Append($"{col:D02}");
+        sb.AppendLine();
+        for (int row = RowMax + 1; row >= RowMin - 1; row--)
+        {
+            sb.Append($"{row:D02} ");
+            for (int col = ColMin - 1; col <= ColMax + 1; col++)
+            {
+                var state = GetCellState(row, col);
+                switch (state)
+                {
+                    case CellState.EmptyIsolated:
+                        sb.Append("  ");
+                        break;
+
+                    case CellState.PotentiallyPlayable:
+                        if (checkTileCompat != null && IsCompatible(row, col, checkTileCompat))
+                        {
+                            if (color)
+                                sb.Append(ConsoleColorCyan);
+                            sb.Append("x ");
+                        }
+                        else
+                        {
+                            if (color)
+                                sb.Append(ConsoleColorDarkGray);
+                            sb.Append("· ");
+                        }
+                        if (color)
+                            sb.Append(ConsoleColorDefault);
+                        break;
+
+                    case CellState.Tiled:
+                        Tile t = GetTile(row, col) ?? throw new Exception("GetTile shouldn't return null");
+                        if (color)
+                            sb.Append(t.Color switch
+                            {
+                                Color.Red => ConsoleColorRed,
+                                Color.Orange => ConsoleColorDarkYellow,
+                                Color.Yellow => ConsoleColorYellow,
+                                Color.Green => ConsoleColorGreen,
+                                Color.Blue => ConsoleColorBlue,
+                                Color.Purple => ConsoleColorMagenta,
+                                _ => ConsoleColorDefault
+                            });
+                        sb.Append(t.Shape switch
+                        {
+                            Shape.Circle => "A",
+                            Shape.Cross => "B",
+                            Shape.Lozange => "C",
+                            Shape.Square => "D",
+                            Shape.Star => "E",
+                            Shape.Clover => "F",
+                            _ => "🞌 "
+                        });
+                        if (color)
+                            sb.Append(ConsoleColorDefault);
+                        sb.Append(' ');
+                        break;
+                }
+            }
+            sb.AppendLine();
+        }
+        return sb.ToString();
+    }
+}
--- 07_Qwirkle/v2/Q2/Program.cs
+++ 07_Qwirkle/v2/Q2/Program.cs
@@ -1,4 +1,11 @@
-﻿using System.ComponentModel;
+﻿// Q2
+// Qwirkle reboot
+//
+// 2023-11-23   PV
+
+using System.Diagnostics;
+using System.Text;
+using LibQwirkle;
 
 namespace Q2;
 
@@ -6,6 +13,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         var b = new Board();
 
         var t1 = new Tile(Color.Red, Shape.Square, 1);
@@ -16,162 +25,32 @@ internal class Program
         var t6 = new Tile(Color.Orange, Shape.Circle, 1);
 
         b.AddMove(new Move(50, 50, t1));
-        b.AddMove(new Move(51, 50, t2));
-        b.AddMove(new Move(52, 50, t3));
-        b.AddMove(new Move(52, 51, t4));
-        b.AddMove(new Move(52, 52, t5));
-        b.AddMove(new Move(52, 53, t6));
+        b.AddMove(new Move(50, 51, t2));
+        b.AddMove(new Move(50, 52, t3));
+        b.AddMove(new Move(49, 52, t4));
+        b.AddMove(new Move(48, 52, t5));
+        b.AddMove(new Move(47, 52, t6));
 
         b.Print();
-    }
-}
 
-enum Shape
-{
-    Circle,
-    Cross,
-    Lozange,
-    Square,
-    Star,
-    Clover,
-}
+        string s = "   4950515253\r\n" +
+                   "51   · · x   \r\n" +
+                   "50 · D C A · \r\n" +
+                   "49   · · A x \r\n" +
+                   "48     x A x \r\n" +
+                   "47     x A x \r\n" +
+                   "46       x   \r\n";
+        string s2 = b.AsString(false, new Tile(Color.Purple, Shape.Circle, 2));
+        Debug.Assert(s == s2);
 
-enum Color
-{
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Purple,
-}
-
-record Tile(Color c, Shape s, int instance)
-{
-    public Color Color { get; } = c;
-    public Shape Shape { get; } = s;
-    public int Instance { get; } = instance;
-}
-
-record Move(int row, int col, Tile t)
-{
-    public int Row { get; } = row;
-    public int Col { get; } = col;
-    public Tile Tile { get; } = t;
-}
-
-enum CellStateType
-{
-    EmptyIsolated,
-    EmptyPlayable,
-    EmptyNotPlayable,
-    Tiled,
-}
-
-enum ColorConstraintType
-{
-    NoConstraint,
-    FixedColor,
-    MultiColor,
-}
-
-enum ShapeConstraintType
-{
-    NoConstraint,
-    FixedShape,
-    MultiShape,
-}
-
-record ColorConstraint(ColorConstraintType type, Color? fixedColor, HashSet<Color>? takenColors)
-{
-    ColorConstraintType Type { get; } = type;
-    Color? FixedColor { get; } = fixedColor;
-    HashSet<Color>? TakenColors { get; } = takenColors;
-
-}
-
-record ShapeConstraint(ShapeConstraintType type, Shape? fixedShape, HashSet<Shape>? takenShapes)
-{
-    ShapeConstraintType Type { get; } = type;
-    Shape? FixedShape { get; } = fixedShape;
-    HashSet<Shape>? TakenShapes { get; } = takenShapes;
-
-}
-
-record CellStatus(CellStateType type, Tile? tile = null, ColorConstraint? nc = null, ColorConstraint? sc = null, ColorConstraint? ec = null, ColorConstraint? wc = null, ShapeConstraint? ns = null, ShapeConstraint? ss = null, ShapeConstraint? ee = null, ShapeConstraint? ws = null)
-{
-    public CellStateType Type { get; } = type;
-    public Tile? Tile;
-    public ColorConstraint? NColorConstraint, SColorConstraint, EColorConstraint, WColorConstraint;
-    public ShapeConstraint? NShapeConstraint, SShapeConstraint, EShapeConstraint, WShapeConstraint;
-}
-
-class Board
-{
-    Board? BaseBoard = null;
-    List<Move> Moves = new List<Move>();
-
-    public int RowMin { get; private set; } = 100;
-    public int RowMax { get; private set; } = 0;
-    public int ColMin { get; private set; } = 100;
-    public int ColMax { get; private set; } = 0;
-
-    public void AddMove(Move m)
-    {
-        Moves.Add(m);
-        RowMin = Math.Min(RowMin, m.Row);
-        RowMax = Math.Max(RowMax, m.Row);
-        ColMin = Math.Min(ColMin, m.Col);
-        ColMax = Math.Max(ColMax, m.Col);
-    }
-
-    public Tile? GetTile(int row, int col)
-    {
-        foreach (var t in Moves)
-            if (t.Row == row && t.Col == col)
-                return t.Tile;
-        if (BaseBoard == null)
-            return null;
-        return BaseBoard.GetTile(row, col);
-    }
-
-    public CellStatus GetCellState(int row, int col)
-    {
-        Tile? t = GetTile(row, col);
-        if (t != null)
-            return new CellStatus(CellStateType.Tiled, tile: t);
-
-        Tile? tn = GetTile(row - 1, col);
-        Tile? ts = GetTile(row + 1, col);
-        Tile? te = GetTile(row, col + 1);
-        Tile? tw = GetTile(row, col - 1);
-        if (tn == null && ts == null && te == null && tw == null)
-            return new CellStatus(CellStateType.EmptyIsolated);
-
-        ColorConstraint? nc = null;
-        ColorConstraint? sc = null;
-        ColorConstraint? ec = null;
-        ColorConstraint? wc = null;
-        ShapeConstraint? ns = null;
-        ShapeConstraint? ss = null;
-        ShapeConstraint? ee = null;
-        ShapeConstraint? ws = null;
-
-        if (tn != null)
-            (nc, ns) = GetConstraints(row, col, -1, 0);
-        if (ts != null)
-            (sc, ss) = GetConstraints(row, col, 1, 0);
-    }
-
-    internal void Print()
-    {
-        for (int row = RowMax; row >= RowMin; row--)
-        {
-            for (int col = ColMin; col <= ColMax; col++)
-            {
-
-            }
-            Console.WriteLine();
-        }
+        s = "   4950515253\r\n" +
+            "51   · x ·   \r\n" +
+            "50 · D C A · \r\n" +
+            "49   · · A · \r\n" +
+            "48     x A x \r\n" +
+            "47     · A · \r\n" +
+            "46       ·   \r\n";
+        s2 = b.AsString(false, new Tile(Color.Green, Shape.Lozange, 2));
+        Debug.Assert(s == s2);
     }
 }
